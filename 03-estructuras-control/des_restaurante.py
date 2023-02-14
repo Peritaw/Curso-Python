a0e27a1803ee70ec283a193df6bc08b175ea87e0
Peritaw <stollerluis@gmail.com>
2023-02-14 13:55:23 -0300

    Update des_restaurante.py

--- 03-estructuras-control/des_restaurante.py
+++ 03-estructuras-control/des_restaurante.py
@@ -1,28 +1,41 @@
 #Entrada
 consumo = float(input('Ingrese el consumo del cliente: '))
 
+
 #Proceso
-if consumo <= 100:
-    #Descuento 10%
-    dato_descuento = '10%'
-    descuento = consumo * 0.10
-elif consumo > 100:
-    #Descuento 20%
-    dato_descuento = '20%'
-    descuento = consumo * 0.20
+if consumo >=0:
+    if consumo <= 100:
+        #Descuento 10%
+        dato_descuento = '10%'
+        descuento = consumo * 0.10
+    elif consumo > 100 and consumo <= 200:
+        #Descuento 20%
+        dato_descuento = '20%'
+        descuento = consumo * 0.20
+    elif consumo > 200:
+        #Descuento 30%
+        dato_descuento = '30%'
+        descuento = consumo * 0.30
 
-monto_descuento = consumo - descuento
+    monto_descuento = consumo - descuento
 
-igv = monto_descuento * 0.19
-total_pagar = monto_descuento + igv
+    igv = monto_descuento * 0.19
+    total_pagar = monto_descuento + igv
 
-#Salida de datos 
-print('='*30)
-print('----- FACTURA DE CONSUMO -----')
-print('='*30)
-print('Consumo: ', consumo)
-print('Descuento: ', descuento)
-print('Monto con descuento: ', monto_descuento)
-print('IGV: ', igv)
-print('Total a pagar: ', total_pagar)
-print('='*30)
+    #Salida de datos 
+    print('='*30)
+    print('----- FACTURA DE CONSUMO -----')
+    print('Descuento que se aplica: ', dato_descuento)
+    print('='*30)
+    print('Consumo: ', consumo)
+    print('Descuento: ', descuento)
+    print('Monto con descuento: ', monto_descuento)
+    print('IGV: ', igv)
+    print('Total a pagar: ', total_pagar)
+    print('='*30)
+
+else:
+    print('Error al ingresar el consumo')
+
+
+
